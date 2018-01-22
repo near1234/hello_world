11716668ad5bfccf79f4a8d291daf1538a816153
near1234 <near@wegwerfemail.de>
2018-01-22 22:34:52 +0100

    clickcounter with gui

--- gui.py
+++ gui.py
@@ -1,1 +1,17 @@
 from tkinter import *
+
+i = 0
+
+
+def increment():
+    global i
+    i = i + 1
+    Label1.config(text=i)
+
+
+master = Tk()
+Label1 = Label(master, text="0")
+Button1 = Button(master, text="Benjamin Button", command=increment)
+Button1.pack()
+Label1.pack()
+mainloop()
